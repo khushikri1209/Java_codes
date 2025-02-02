459ec2780e5679dbe94f0f497bd0fb9fc35e27b8
khushikri1209 <khushikumari12029@gmail.com>
2025-02-02 22:47:09 +0530

    added

---
+++ 01Basic/J01Hello.java
@@ -0,0 +1,6 @@
+public class J01Hello {
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+    }
+    
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Java_codes
-Java programming Codes example
