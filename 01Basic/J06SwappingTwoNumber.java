0160dbacb914feb339736e17a3f996957d19d109
khushikri1209 <khushikumari12029@gmail.com>
2025-02-13 18:58:16 +0530

    change

--- 01Basic/J06SwappingTwoNumber.java
+++ 01Basic/J06SwappingTwoNumber.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class J06SwappingTwoNumber {
-    public static void main(String[]args){
-        
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("before swapping");
+        System.out.println("Enter first number: ");
+        int first = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int second = sc.nextInt();
+        int temp = first;
+        first = second;
+        second = temp;
+        System.out.println("after swapping");
+        System.out.println("first number: " + first);
+        System.out.println("second number: " + second);
+
     }
-    
+
 }
+
+
