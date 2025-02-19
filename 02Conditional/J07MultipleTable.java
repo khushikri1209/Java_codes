ecf8dedb86e016100d2a10f04853b28fbfbc957d
khushikri1209 <khushikumari12029@gmail.com>
2025-02-19 12:36:04 +0530

    Update J07MultipleTable.java

--- 02Conditional/J07MultipleTable.java
+++ 02Conditional/J07MultipleTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class J07J0MultipleTable {
+public class J07MultipleTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter table: ");
