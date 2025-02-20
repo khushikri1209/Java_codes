adc3ad9eed19aaa32220b5749813b3eb9c278418
khushikri1209 <khushikumari12029@gmail.com>
2025-02-20 15:45:40 +0530

    Update J11ProfitLoss.java

---
+++ 02Conditional Programs/J11ProfitLoss.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class J11ProfitLoss
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter The Cost Price :");
+		int cp = input.nextInt();
+		System.out.println("Enter The Selling Price :");
+		int sp = input.nextInt();
+		int p,l;		
+		if(sp>cp)
+		{
+			p = sp-cp;
+			System.out.println("Profit : "+p);
+		}
+		else if(sp<cp)
+		{
+			l = cp-sp;
+			System.out.println("Loss : "+l);	
+		}
+		else
+		{
+			System.out.println("No Profit No Loss");
+		}
+	}
+}
