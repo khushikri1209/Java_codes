9f03c3639cc3bd306c1c3184380a7497b5f6027d
khushikri1209 <khushikumari12029@gmail.com>
2025-02-21 09:48:49 +0530

    Create J16Age.java

---
+++ 02Conditional Programs/J16Age.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+public class age {
+    public static void main(String args[]) {
+        int age;
+        System.out.println("Enter Your Age : ");
+        Scanner in = new Scanner(System.in);
+        age=in.nextInt();
+        if(age>=18)
+        {
+            System.out.println("You are Eligible For Vote...");
+        }
+    }
+}
