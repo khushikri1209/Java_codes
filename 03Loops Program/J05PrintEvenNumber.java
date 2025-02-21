2e4d7fcf5a1eea3caa5f1954638b9fc7c82fbf1f
khushikri1209 <khushikumari12029@gmail.com>
2025-02-21 14:57:00 +0530

    Create J05PrintEvenNumber.java

---
+++ 03Loops Program/J05PrintEvenNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class J05printEvenNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Number of Limit : ");
+		int l =input.nextInt();
+		for(int s=1;s<=l;s++)
+		{
+			if(s%2==0)
+				System.out.println(s);
+		}
+	}
+}
