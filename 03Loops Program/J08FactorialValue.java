9d582c6ecc4b384d2bf2bdc426f69f0dd09f6eae
khushikri1209 <khushikumari12029@gmail.com>
2025-02-22 14:18:17 +0530

    Create J08FactorialValue.java

---
+++ 03Loops Program/J08FactorialValue.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class J08FactorialValue  
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Factorial Number : ");
+		int num = input.nextInt();
+		int fact = 1;
+		for(int i=1; i<=num; i++)
+		{
+			fact *= i;
+		}        
+		System.out.println("Factorial: "+ fact);		
+	}
+}
+
