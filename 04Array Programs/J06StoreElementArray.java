d42d5af0f3d70dd3d4d93b83fb0d9ae70c582fe4
khushikri1209 <khushikumari12029@gmail.com>
2025-02-25 20:25:16 +0530

    Create J06StoreElementArray.java

---
+++ 04Array Programs/J06StoreElementArray.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class J06StoreElementArray
+{
+	public static void main(String[] args)
+	{   
+		Scanner input =new Scanner(System.in);
+		int [] a =new int[7]; 
+		for(int i=0;i<7;i++)
+		{
+			System.out.printf("Element of a[%d] :",i);
+			a[i]=input.nextInt();
+		} 
+		System.out.println("\nDisplay Array Elements...\n");
+		for(int e:a)
+		{
+			System.out.println(e);
+		}
+    }
+}
