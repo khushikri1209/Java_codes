e0f3bb9b2f277dcb39dac729d248b87d55ac6af9
khushikri1209 <khushikumari12029@gmail.com>
2025-02-25 12:41:21 +0530

    Create J03AverageOfSumOfArray.java

---
+++ 04Array Programs/J03AverageOfSumOfArray.java
@@ -0,0 +1,13 @@
+public class J03AverageOfSumOfArray {
+    public static void main(String[]args){
+        int i,sum=0,avg=0;
+        int a[]={10,20,30};
+        for(i=0;i<a.length;i++){
+            sum+=a[i];
+
+        }
+        avg=sum/a.length;
+        System.out.println("Average :"+avg);
+    }
+    
+}
