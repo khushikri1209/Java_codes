aa4cfc96bd3c8bd7e30c9a61a2a9703b0dcb3730
khushikri1209 <khushikumari12029@gmail.com>
2025-02-25 12:25:14 +0530

    Create J01simpleStaticArray.java

---
+++ 04Array Programs/J01simpleStaticArray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class J01simpleStaticArray {
+    public static void main(String[]args){
+        int a[]={10,20,30,40,50};
+        for(int i=0;i<5;i++){
+            System.out.println(a[i]);
+        }
+    }
+    
+}
