284a2dac1898d197373e6808aa056c55f557f8cd
khushikri1209 <khushikumari12029@gmail.com>
2025-02-26 11:21:55 +0530

    Create J08CubicValueArray.java

---
+++ 04Array Programs/J08CubicValueArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class J08CubicValueArray {
+	public static void main(String[] args)
+	{   
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter the Array Limit :");
+		int l =input.nextInt();
+		int [] a =new int[l];
+		for(int i=0;i<l;i++)
+		{
+			System.out.printf("Element of a[%d] :",i);
+			a[i]=input.nextInt();
+		}
+		System.out.println("\nCubic Array Elements...\n");
+		for(int e:a)
+		{
+			System.out.println(e*e*e);
+		}
+    }
+}
+
+    
+
