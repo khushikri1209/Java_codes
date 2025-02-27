d944da7c0991b6a34ac7bb0854c6b51f1b8218ec
khushikri1209 <khushikumari12029@gmail.com>
2025-02-27 17:27:57 +0530

    Create J22PalindromeOrNot.java

---
+++ 03Loops Program/J22PalindromeOrNot.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class J22PalindromeOrNot
+{
+	public static void main (String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter The Digits :");
+		int num = input.nextInt();
+		int rev = 0, rem, temp;
+		temp = num;
+		while (num != 0)
+		{
+			rem = num % 10;
+			rev = rev * 10 + rem;
+			num /= 10;
+		};
+ 
+		if (temp == rev)
+			System.out.println (temp + " is Palindrome");
+		else
+			System.out.println (temp + " is not Palindrome");
+	}
+}
+
