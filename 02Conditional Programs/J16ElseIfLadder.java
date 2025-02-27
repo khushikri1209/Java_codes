2d8cd0a7a9bbbe97707fa5339c8e4d53d4747e65
khushikri1209 <khushikumari12029@gmail.com>
2025-02-27 21:03:05 +0530

    Create J16ElseIfLadder.java

---
+++ 02Conditional Programs/J16ElseIfLadder.java
@@ -0,0 +1,25 @@
+public class J16ElseIfLadder {
+    public static void main(String args[]){
+
+        int a = 13;
+
+        if (a < 0) {
+            System.out.println("a is negative");
+        } else if (a == 0) {
+            System.out.println("a is zero");
+        } else if (a > 0) {
+            System.out.println("a is positive");
+        }
+    }
+}
+     
+
+        
+
+       
+    
+      
+
+    
+      
+
