c5d3ee37c0d2c699487bf2c1e4593e30026db64c
khushikri1209 <khushikumari12029@gmail.com>
2025-03-01 21:44:48 +0530

    Update J03StarPattern.java

--- 05Pattern Program/J03StarPattern.java
+++ 05Pattern Program/J03StarPattern.java
@@ -1,8 +1,36 @@
 import java.util.*;
+
 public class J03StarPattern {
-    public static void main(String args[]){
-        
-        
+    public static void printStars(int n) {
+
+        int i, j;
+
+        for (i = 0; i < n; i++) {
+
+            for (j = 2 * (n - i); j >= 0; j--) { // For Loop for Row
+
+                System.out.print(" "); // Print Spaces
+
+            }
+
+            for (j = 0; j <= i; j++) { // For Loop for col
+
+                System.out.print("* "); // Print Star
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        int n = 5; // Number of Rows
+
+        printStars(n);
+
     }
 
 }
