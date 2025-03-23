6da303c0118eacca84dbb9e8b37e9da4ab2bdbb7
khushikri1209 <khushikumari12029@gmail.com>
2025-03-23 06:55:51 +0530

    Update J17ArmstrongNumber.java

--- 01Basic Programs/J17ArmstrongNumber.java
+++ 01Basic Programs/J17ArmstrongNumber.java
@@ -1,5 +1,35 @@
 public class J17ArmstrongNumber {
-    public static void main(String args[]){
-        
+    // Java Program to Check Armstrong Number between Two Integers
+    static void ArmstrongNum(int l, int h) {
+        for (int j = l + 1; j < h; ++j) {
+
+            // Calculating number of digits
+            int y = j;
+            int N = 0;
+            while (y != 0) {
+                y /= 10;
+                ++N;
+            }
+
+            // Calculating the sum of nth
+            // power of all the digits
+            int sum_power = 0;
+            y = j;
+            while (y != 0) {
+                int d = y % 10;
+                sum_power += Math.pow(d, N);
+                y /= 10;
+            }
+
+            // Checking if the current number
+            // i is equal to the sum of nth
+            // power of all the digits
+            if (sum_power == j)
+                System.out.print(j + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+
     }
 }
