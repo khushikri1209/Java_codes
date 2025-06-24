857ec76a95a3e6d2b03fbfeb425f01ef37515909
khushikri1209 <khushikumari12029@gmail.com>
2025-06-24 19:40:15 +0530

    Create J07StringDouble.java

---
+++ 06Conversion Program/J07StringDouble.java
@@ -0,0 +1,15 @@
+public class J07StringDouble {
+    public static void main(String[] args) {
+        // Create and initialize a string
+        String str = "2033.12244";
+
+        // Converting the above string into Double using parseDouble() Method
+        double str1 = Double.parseDouble(str);
+
+        // Printing string as Double type
+        System.out.println(str1);
+    }
+}
+
+    
+
