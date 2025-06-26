f6017ac56dcfd9def4c85c828b78ced610063609
khushikri1209 <khushikumari12029@gmail.com>
2025-06-26 20:56:53 +0530

    Create J09StringToLong.java

---
+++ 06Conversion Program/J09StringToLong.java
@@ -0,0 +1,19 @@
+public class J09StringToLong 
+
+{    public static void main(String[] args) {
+
+        // Step 1: Creating a string containing numeric characters
+        String s = "999999999999";
+        System.out.println("String: " + s);
+
+        // Step 2: Converting the string into a long value
+        long l = Long.parseLong(s);
+
+        // Step 3: Printing the converted long value
+        System.out.println("Long: " + l);
+    }
+}
+
+
+    
+
