076557a6aebec240c8813418c043f7dd10057b19
khushikri1209 <khushikumari12029@gmail.com>
2025-06-28 23:09:11 +0530

    Create J11IntToChar.java

---
+++ 06Conversion Program/J11IntToChar.java
@@ -0,0 +1,12 @@
+public class J11IntToChar {
+    public static void main(String[] args) {
+        // Custom integer input
+        int i = 97;
+
+        // Type casting character to integer
+        char ch = (char) i;
+
+        // Print statement
+        System.out.println(ch);
+    }
+}
